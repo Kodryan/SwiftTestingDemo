3c434755cf070e2d6cdbd846d1f45ecf2778f1a9
Dimitry Kodryan <kodryan@tutu.tech>
2025-03-28 19:45:11 +0100

    Initial Commit

---
+++ SwiftTestingDemo/SwiftTestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingDemoApp.swift
+//  SwiftTestingDemo
+//
+//  Created by Kodryan Dmitriy on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTestingDemoTests/SwiftTestingDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingDemoTests.swift
+//  SwiftTestingDemoTests
+//
+//  Created by Kodryan Dmitriy on 28.03.2025.
+//
+
+import Testing
+@testable import SwiftTestingDemo
+
+struct SwiftTestingDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
